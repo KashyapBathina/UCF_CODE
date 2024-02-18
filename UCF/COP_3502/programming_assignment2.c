6cdb35d2eed3ef099742d78c665f1b3b3c75160b
kashyapbathina <kbathina05@gmail.com>
2024-02-17 20:13:53 -0500

    Adding program assignmnet 2

---
+++ UCF/COP_3502/programming_assignment2.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/*
+    name: Kashyap Bathina
+    date: 2/17/24
+    purpose: 
+*/
+
+
+
+int main () {
+    // 
+    printf("hello, world\n");
+
+    return 0;
+}
