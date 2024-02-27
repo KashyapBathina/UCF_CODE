181d68a5cab08bb7df4b21edbafb6cff054e64df
kashyapbathina <kbathina05@gmail.com>
2024-02-27 01:39:15 -0500

    3456

--- UCF/COP_3502/b.c
+++ UCF/COP_3502/b.c
@@ -11,7 +11,7 @@ dynamic:
         syntax: int *p = (int *) malloc(sizeof(int)); casting null pointer to int pointer with (int *), b/c malloc returns null pointer, that points to that address
     calloc: void *calloc(size_t n, size_t e); allocates array of size n with each element of size e; unlike malloc, each space initialized to 0; int *p = (int *) calloc(1, sizeof(int));
     memory leak: int *p1 = malloc . . .; int *p2 = malloc . . .; for(loop) {scan into &p1[i]} p2 = p1; return p1: memory leak, b/c after p2=p1, both pointers are pointing to p2; allocated
-        memory for p2 got disconnected since we don't know adress anymore and still alive, but no longer able to freed and just takes space
+        memory for p1 got disconnected since we don't know adress anymore and still alive, but no longer able to freed and just takes space
     realloc: when allocated size ! enough: wrong approach is to allocate new mem and copy old to new and free old; alternative is realloc: void *realloc(void *ptr, size_t size), where
         ptr is old pointer and size is new size. realloc allocates size amount of bytes and copies the content from the allocated data in ptr and returns void pointer, for example, if
         values is an int pointer and already allocated to numVals size, then realloc with values = (int*)realloc(values,(numVals+EXTRA)*sizeof(int));
