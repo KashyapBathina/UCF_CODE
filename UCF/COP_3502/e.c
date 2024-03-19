5a60b7e242d0356aa1cf82fc2314ea910e97b80c
kashyapbathina <kbathina05@gmail.com>
2024-03-18 21:12:33 -0400

    sdf

--- UCF/COP_3502/e.c
+++ UCF/COP_3502/e.c
@@ -19,6 +19,22 @@ algorithm analysis:
             T(4) = c*4^2 = 10 -> c=10/16; so T(16) = (10/16) * 16^2 = 10 * 16 = 160 ms; (note for solving: log_2(16) = log_2(2^4) = 4)
         code example: int i&j=0; while (i<n) {while (j<n && arr[i][j]==1) { j++} } i++; might assume since while loop inside while loop, O(n^2), but instead O(n), b/c in first 
             loop outer, run the inner loop until exhaust n times (since no reset), after that, inner can no longer run so just O(n+n) = O(n); if put j=0 bfore inner, then n^2
+
+recursive relations:
+	to find o(n) for recursive functions: cannot just eyeball like before, must use summations to find an equation that defines a sequence recursively to find big O of recursive
+	code example: int fact (int n) { if(n=1) return 1; else return (n*fact(n-1))}, simple factorial code; where T(n) is # operations, total num operations for fact with any input
+		is the num arithmetic op (-, *) plus the num of operations to execute n-1 times, in other words T(n) = T(n-1) + 2 and T(1) = 1; however, we don't actually know # operats
+		b/c T(n-1) unknow, therefore must find closed form by eliminating functions T(...)'s from RHS, thus reduce T(n-1) till we get T(1) b/c that is the only we value we know,
+		thus we just plug n-1 into the n's until get a pattern (no need to go to T(1) necc): 
+			eq-1) T(n) = T(n-1) + 2     eq-2) T(n-1) = T(n-2) + 2, so T(n) = T()     eq-3) T(n-2) = T(n-3) + 2; then plugging back into T(n) = T(n-3) + 2*3, hence pattern is 
+			T(n) = T(n-k) + 2*k; to solve now, remember T(1) = 1, so we want n-k=1 -> k=n-1, plugging back in -> T(n) = T(n-n+1) + 2*(n-1) = T(1) + 2n-2 = 2n-1, hence O(n)
+	code example: in power(int x, int n) {if n=0, return 1    if n=1, return x    if n is even, return power(x*x, n/2)     if n is odd, return power(x*x,n/2)*x}, to calc x^n,
+		when computing time complexity, we assume worst case, so when odd do three arithmetic operations, so assume always odd -> T(n) = T(n/2) + 3 and T(1) = 1, then to find the
+		closed form: 
+			eq-1) T(n) = T(n/2) + 3     eq-2) T(n/2) = T(n/4) + 3     eq-3) T(n/4) = T(n/8) + 3, then subsituting T(n) = T(n/8) + 2*3 or T(n) = T(n/2^k) + 3*k, we want n/2^k=1, as
+			thats the only T(..) we know, so n/2^k = 1 and n=2^k, we also want k, so log(n) = log(2^k) -> log(n) = k, so T(n) = T(2^k/2^k) + 3*log(n) = 1 + 3logn, so O(logn)
+	note: we consider arithmetic operations here while we don't in the algorithm analysis b/c for loops, the num arithmetic operations is constant, so just O(kn) -> O(n) and just
+	depends on and scales with n; for recursion, the num arithmetic operations per call depends on the n and so the operations varies, thus must consider recursiv calls and operat
 */
 
 #include<stdio.h>
