0cf85ae9dfa89eef9a8d0093c5f3cb7015641b54
kashyapbathina <kbathina05@gmail.com>
2024-06-04 12:45:13 -0400

    Editing

--- UCF/COP_3330/src/assignment1/Bathina_Kashyap.java
+++ UCF/COP_3330/src/assignment1/Bathina_Kashyap.java
@@ -3,6 +3,14 @@ package assignment1;
 import java.util.Scanner;
 import java.util.Random;
 
+/*
+Programming Assignment 1 COP 3330
+	This program simulates shopping, printing a receipt with random details, then using user input until the user types in done, to get items bought, their price
+	and then calculating the price with the tax rate--where food items are un-taxed-- of 30% with each item, then printing the total number of items and price at 
+	the end.  
+ */
+
+
 public class Bathina_Kashyap {
 
 	public static void main(String[] args) {
@@ -11,15 +19,15 @@ public class Bathina_Kashyap {
 		Random rand = new Random();
 		
 		// output layout
-		System.out.println("******************************\n"
-				+ "****** S store ***************\n"
-				+ "******************************");
+		System.out.println("******************************\n" +
+							"****** S store ***************\n" +
+							"******************************");
 		
-		// gets random recipient number
+		// gets random recipient number between 1000 and 2000
 		int recipientNumber = rand.nextInt(1000)+1000; 
-		System.out.println("recipient number      " + recipientNumber);
+		System.out.println("receipt number      " + recipientNumber);
 		
-		// gets random date, getting month by randomizing array of all months
+		// gets random date: getting month by randomizing array of all months, getting random year between 1900 and 2100, getting random day between 1 and 28
 		String[] months = {"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
 		String month = months[rand.nextInt(12)];
 		int year = rand.nextInt(200)+1900;
@@ -30,16 +38,18 @@ public class Bathina_Kashyap {
 		int itemCount = 0;
 		double totalPrice = 0.0;
 		while (true) {
-			// getting item name
+			// getting item name using scanner and also capitalizing 1st letter
 			System.out.print("Write item name ");
 			String itemName = sc.nextLine();
+			if (!itemName.equals(""))
+				itemName = itemName.substring(0,1).toUpperCase() + itemName.substring(1);
 			
 			// if user types in done, break the loop 
 			if (itemName.equals("DONE")) {
 				break;
 			}
 			
-			// getting price of item
+			// getting price of item with scanner
 			double price = 0;
 			System.out.print("Write price ");
 			double itemPrice = sc.nextDouble();
@@ -65,7 +75,7 @@ public class Bathina_Kashyap {
 							"******************************\n" +
 							"******************************\n");
 		
-		
+		// closing scanner 
 		sc.close();
 	}
 
